7a171b1c83a31ccf5f9792add4aa7097c6fd7768
root <root@DESKTOP-8Q58I4D.localdomain>
2026-06-23 03:15:19 +0200

    Add a new .c file 4-puts.c

---
+++ hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#stdio.h
+
+/**
+ * main - Prints a string of characters using puts and not printf
+ * Return: 0 to exit program after the text printing
+ */
+int main(void)
+{
+	puts("Programing is like building a multilingual puzzle\n");
+	return (0);
+}
