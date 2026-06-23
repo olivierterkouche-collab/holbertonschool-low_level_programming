4c71ccf5109c1dd5546a3a3a867b8fe5c8b543ba
root <root@DESKTOP-8Q58I4D.localdomain>
2026-06-23 05:42:42 +0200

    Add new .c file 6-size.c

---
+++ hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Each printf command prints a text includind the size of the type
+ * it talks about
+ * Retrun: Returns 0 if everythings works as planned
+ */
+int main(void)
+{
+	printf("Size of a char: %zu byte(s)\n"sizeof(char));
+	printf("Size of a int: %zu byte(s)\n"sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n"sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n"sizeof(long long int));
+	printf("Size of a float: %zu bytes(s)\n"sizeof(float));
+	return (0);
+}
