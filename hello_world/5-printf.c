9c3bdb0ef1134d768eb6871ac44abd6eab33d4d1
root <root@DESKTOP-8Q58I4D.localdomain>
2026-06-23 04:03:24 +0200

    Add new .c file 5-printf.c

---
+++ hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - prints a string of characters unsing printf and not puts this time
+ * Return: returns the value 0 when the text has been sucessfully printed
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
