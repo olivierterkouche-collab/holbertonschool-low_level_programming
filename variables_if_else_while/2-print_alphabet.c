18afbea8c5c4b8514a6ba89a4b0afc043ea843fd
root <root@DESKTOP-8Q58I4D.localdomain>
2026-06-24 04:10:33 +0200

    Add new .c file 2-print_alphabet.c

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - A for loop that prints the alphabet in lower case
+ * Return: Returns 0 if the code worked as intended
+ */
+int main(void)
+{
+	for (i=97; i <= 122; i++)
+	{
+	putchar(i);
+	}
+	return (0);
+}
