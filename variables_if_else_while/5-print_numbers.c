ea9704d2eab80386c70d86cdf15b98a19a1c55ba
root <root@DESKTOP-8Q58I4D.localdomain>
2026-06-24 09:56:08 +0200

    Add new .c file 5-print_numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints digits from 0 to 9 ending with a line break
+ * Return: Returns the value 0 if the code is executed properly
+ */
+int main(void)
+{
+int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+	printf("%d", n);
+		if (i == 9)
+		{
+		printf("\n")
+		}
+	}
+	return (0);
+}
