64f502825e2f2e07332a1db25c154bf32175d5ad
root <root@DESKTOP-8Q58I4D.localdomain>
2026-06-24 10:54:01 +0200

    Add new .c file to 7-print_tebahpla.c

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - For loop that prints the alphabet in reverse and lowercase
+ * then breaks to a new line
+ * Return: Returns 0 if the code worked as intended
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 122; i >= 97; i--)
+	{
+	putchar(i);
+		if(i == 97)
+		{
+		putchar(10)
+		}
+	}
+	return (0);
+}
