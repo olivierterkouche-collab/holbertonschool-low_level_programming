b011dabb8d2a2193d14aacd436a839c47fd6c5f2
root <root@DESKTOP-8Q58I4D.localdomain>
2026-06-24 14:14:57 +0200

    Rewrited the script to add a line break at the end of the code in file 9-print_comb.c

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -11,15 +11,15 @@ int i;
 
 	for (i = 48; i <= 57; i++)
 	{
+	putchar(i);
 		if (i < 57)
 		{
-		putchar(i);
 		putchar(44);
 		putchar(32);
 		}
 		else
 		{
-		putchar(i);
+		putchar(10);
 		}
 	}
 	return (0);
