933a85ed0482bf60f05473bc1463e048025c0eb9
root <root@DESKTOP-8Q58I4D.localdomain>
2026-06-24 03:36:12 +0200

    Changed else if (lastDigit < 6 && != O) for else if (lastDigit < 6 && lastDigit != O) in file 1-last_digit.c

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -19,7 +19,7 @@ int main(void)
 	{
 	printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
 	}
-	else if (lastDigit < 6 && != O)
+	else if (lastDigit < 6 && lastDigit != O)
 	{
 	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
 	}
