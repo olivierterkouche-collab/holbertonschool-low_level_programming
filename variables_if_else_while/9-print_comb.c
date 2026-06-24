3cb56f267137aee29e266e73ef3d0f699f7bf5a5
root <root@DESKTOP-8Q58I4D.localdomain>
2026-06-24 12:59:39 +0200

    Add new .c file 9-print_comb.c

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all decimal digits folowed by a coma and a space
+ * Return: Returns 0 if code was executed correctly
+ */
+
+int main(void)
+{
+int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+	putchar(i);
+	putchar(32);
+	putchar(44);
+	}
+	return (0);
+}
