2c9bb344d69e8098f52c97e626eaea10bcd61bf0
root <root@DESKTOP-8Q58I4D.localdomain>
2026-06-25 11:12:00 +0200

    Add new C file 1-alphabet.c and new fonction declaration in main.h

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints the alphabet then adds a new line
+ * Return: Returns 0 if the fonction is executed correctly
+ */
+
+void print_alphabet(void)
+{
+int i;
+	for (i = 97; i <= 122; i++)
+	{
+	_putchar(i);
+		if (i == 122)
+		{
+		_putchar(10);
+		}
+	}
+	return (0);
+}
