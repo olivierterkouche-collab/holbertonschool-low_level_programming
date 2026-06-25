bfab977a8c5e85b40d469f99e2e674b14249e119
root <root@DESKTOP-8Q58I4D.localdomain>
2026-06-25 15:04:28 +0200

    Add new C file 2-print_alphabet_x10.c and added a new function in header file main.h

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints the alphabet 10 times then line breaks
+ */
+
+void print_alphabet_x10(void)
+{
+char i;
+char j;
+i = 1;
+	while (i <= 10)
+	{
+		for (j = 97; j <= 122; j++)
+		{
+		_putchar(j);
+		}
+		if (i == 10)
+		{
+		_putchar(10);
+		}
+	i++;
+	}
+}
