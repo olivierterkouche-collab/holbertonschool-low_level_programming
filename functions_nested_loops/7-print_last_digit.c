f23415a29be0cdb2710387d0a1606b2869087d37
root <root@DESKTOP-8Q58I4D.localdomain>
2026-06-26 10:30:43 +0200

    Add new C file 7-print_last_digit.c also updated main.h header file

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of the parametters input
+ * @r: parameters input used in the function
+ *
+ * Return: Returns the value of r after it has been processed by the function
+ */
+
+int print_last_digit(int r)
+{
+	if (r < 0)
+	{
+	r = r * -1;
+	}
+r = r % 10;
+_putchar(r);
+_putchar(10);
+return (r);
+}
