f870c715e9b02890c7817fc7a9f7fb9cfa5d01b0
root <root@DESKTOP-8Q58I4D.localdomain>
2026-06-26 09:35:33 +0200

    Add new C file 6-abs.c and it's varriable prototype to header main.h

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _abs - Checks if the parametter is a positive number if not makes it one
+ * @int: parametter we want to get the absolute value of
+ *
+ * Return: Returns the absolute value of the parameter
+ */
+
+int _abs(int)
+{
+	if (r < 0)
+	{
+	r = r * -1;
+	return (r);
+	}
+	else
+	{
+	return (r);
+	}
+}
